4ecfbcd92b668f7fedb3c13781cbc35abcef64b6
Kian <kianwang022@gmail.com>
2026-09-11 13:56:38 -0700

    add Cloze answer gate route

---
+++ static-web/src/pages/cloze-answer/[id].json.js
@@ -0,0 +1,20 @@
+import { listClozeSets, loadClozeAnswersById } from '../../lib/englishObjective.mjs';
+
+export const prerender = true;
+
+export function getStaticPaths() {
+  return listClozeSets().map((item) => ({
+    params: { id: item.id },
+    props: { objectId: item.id }
+  }));
+}
+
+export function GET({ props }) {
+  const payload = loadClozeAnswersById(props.objectId);
+  return new Response(JSON.stringify(payload), {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Cache-Control': 'public, max-age=300'
+    }
+  });
+}
